5268b5e08074b0dd61d824e44b8abe3294459877
dulcejazminantunezrios <71049429+dulcejazminantunezrios@users.noreply.github.com>
2020-10-21 19:47:29 -0500

    Cambios para la parte de comentarios

--- app/Http/Controllers/API/ComentarioController.php
+++ app/Http/Controllers/API/ComentarioController.php
@@ -4,8 +4,41 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\modelos\comentarios;
 class ComentarioController extends Controller
 {
     //
+    public function todosc($id=null){
+        if($id)
+            return response()->json(["Comentario"=>comentarios::find($id)],200);
+        return response()->json(["Comentarios"=>comentarios::all()],200);
+    }
+    public function crearc(Request $request){
+        $com=new comentarios;
+        $com->titulo=$request->titulo;
+        $com->cuerpo=$request->cuerpo;
+        $com->producto_id=$request->producto_id;
+
+        if($com->save())
+            return response()->json(["Comentario creado"=>$com],201);
+        return response()->json(null,400);
+
+    }
+    public function actualizarc(Request $request, $id){
+        $com=new comentarios;
+        $com=comentarios::find($id);
+        $com->titulo=$request->titulo;
+        $com->cuerpo=$request->cuerpo;
+        $com->producto_id=$request->producto_id;
+        if($com->save())
+            return response()->json(["Comentario actualizado:"=>$com],201);
+        return response()->json(null,400);
+    }
+    public function borrarc($id){
+        $com=new comentarios;
+        $com=comentarios::find($id);
+        if($com->delete())
+            return response()->json(["Comentario eliminado"=>$com],200);
+        return response()->json(null,400);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//productos
 Route::get("/productos","API\ProductoController@todosp");//R todos los productos sin excepción
-Route::get("/productos/{id?}","API\ProductoController@todosp")->where("id","[0-9]+");//C producto con un id opcional(parametro)
+Route::get("/productos/{id?}","API\ProductoController@todosp")->where("id","[0-9]+");//R muestra producto segun id
 Route::post("/productos","API\ProductoController@crear");//C crear un producto
-Route::put("/productos/{id}","API\ProductoController@actualizar")->where("id","[0-9]+");//U actualizar datos
-Route::delete("productos/{id}","API\ProductoController@borrar");//borrar especifico
+Route::put("/productos/{id}","API\ProductoController@actualizar")->where("id","[0-9]+");//U actualizar producto
+Route::delete("/productos/{id}","API\ProductoController@borrar");//D borrar especifico
+
+//comentarios
+Route::get("/comentarios","API\ComentarioController@todosc");//R todos los comentarios sin excepcion
+Route::get("/comentarios/{id?}","API\ComentarioController@todosc")->where("id","[0-9]+");//R muestra comentario segun id
+Route::post("/comentarios","API\ComentarioController@crearc");//C crear comentario
+Route::put("/comentarios/{id}","API\ComentarioController@actualizarc")->where("id","[0-9]+");// U actualizar comentario
+Route::delete("/comentarios/{id}","API\ComentarioController@borrarc");//D borrar comentario especifico
